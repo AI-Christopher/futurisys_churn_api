c81c928a28137c184d905f9721834f20aa0908cb
Christopher Ramothe <christopher.ramothe@gmail.com>
2025-08-31 15:43:56 +0200

    feat: Création de l'API de prédilection et test de fonctionnement

--- src/futurisys_churn_api/api/main.py
+++ src/futurisys_churn_api/api/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from .endpoints import prediction
 
 app = FastAPI(
     title="Futurisys Turnover Prediction API",
     description="API pour prédire la probabilité de démission d'un employé.",
     version="0.1.0",
 )
+
+app.include_router(prediction.router)
 
 @app.get("/")
 def read_root():
