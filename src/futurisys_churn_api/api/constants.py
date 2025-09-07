215b85c63efaf1296b9fd643d937c9f0e79422eb
Christopher Ramothe <christopher.ramothe@gmail.com>
2025-09-07 09:28:44 +0200

    fix: Correction de plusieurs warning pytest

---
+++ src/futurisys_churn_api/api/constants.py
@@ -0,0 +1,30 @@
+MOYENNES_POSTE = {
+    "Représentant Commercial": 2626.0,
+    "Consultant": 3237.17,
+    "Assistant de Direction": 3239.97,
+    "Ressources Humaines": 4235.75,
+    "Cadre Commercial": 6924.28,
+    "Tech Lead": 7295.14,
+    "Manager": 7528.76,
+    "Directeur Technique": 16033.55,
+    "Senior Manager": 17181.67,
+}
+
+MAPPING_POSTE = {
+    'Représentant Commercial': 0, 
+    'Consultant': 1, 
+    'Assistant de Direction': 2, 
+    'Ressources Humaines': 3, 
+    'Cadre Commercial': 4, 
+    'Tech Lead': 5, 
+    'Manager': 6, 
+    'Directeur Technique': 7, 
+    'Senior Manager': 8
+}
+
+MAPPING_FREQ = {
+    'Aucun': 0, 
+    'Occasionnel': 1, 
+    "Fréquent": 2,
+    "Frequent": 2,
+}
